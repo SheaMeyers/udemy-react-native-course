1ab5c24e2bb8d61e39d8baa1f506f215c7ae902e
Shea Meyers <shea.meyers@gmail.com>
2021-07-18 18:28:08 +0200

    Set up navigation

--- tracks/src/api/tracker.js
+++ tracks/src/api/tracker.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     // Need to update this whenever restarting ngrok
-    baseURL: 'http://ff1f78d7a373.ngrok.io'
+    baseURL: 'http://0bd64c2e9578.ngrok.io'
 })
--- tracks/src/screens/SignupScreen.js
+++ tracks/src/screens/SignupScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
@@ -35,6 +35,13 @@ const SignupScreen = ({ navigation }) => {
                 title="Sign Up" 
                 onPress={() => signup({ email, password })}
             />
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Spacer>
+                    <Text style={styles.link}>
+                        Already have an account? Sign in instead.
+                    </Text>
+                </Spacer>            
+            </TouchableOpacity>
         </View>
     )
 };
@@ -56,6 +63,9 @@ const styles = StyleSheet.create({
         color: 'red',
         marginBottom: 15,
         marginLeft: 15
+    },
+    link: {
+        color: 'blue'
     }
 });
 
